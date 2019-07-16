6fb499a9459cc82da599457f4791135e5f3ea446
PPODO <sherlockwhooo@gmail.com>
2019-07-16 23:02:57 +0900

    모듈
    Log 수정

--- Server-lib/Server-lib/Functions/Log/Log.h
+++ Server-lib/Server-lib/Functions/Log/Log.h
@@ -8,24 +8,18 @@
 
 class CLog {
 private:
-	inline static bool Write(bool bIsUnicode, const void* Data) {
+	inline static bool Write(bool bIsUnicode, const void* LogData) {
 		SYSTEMTIME SysTime;
-		va_list ArgcList;
 
 		GetLocalTime(&SysTime);
 		if (bIsUnicode) {
-			LPCTSTR ArgcData = static_cast<LPCTSTR>(Data);
+			LPCTSTR Log = static_cast<LPCTSTR>(LogData);
 			TCHAR CurrentDate[MAX_DATETIME_LENGTH] = { L"\0" };
 			TCHAR CurrentFileName[MAX_PATH] = { L"\0" };
 			TCHAR DebugLog[MAX_BUFFER_LENGTH] = { L"\0" };
-			TCHAR Log[MAX_BUFFER_LENGTH] = { L"\0" };
-
-			va_start(ArgcList, ArgcData);
-			vswprintf_s(Log, ArgcData, ArgcList);
-			va_end(ArgcList);
 
 			swprintf_s(CurrentDate, MAX_DATETIME_LENGTH, L"%d-%d-%d %d:%d:%d", SysTime.wYear, SysTime.wMonth, SysTime.wDay, SysTime.wHour, SysTime.wMinute, SysTime.wSecond);
-			swprintf_s(CurrentFileName, MAX_PATH, L"LOG_%d-%d-%d %d.log", SysTime.wYear, SysTime.wMonth, SysTime.wDay, SysTime.wHour);
+			swprintf_s(CurrentFileName, MAX_PATH, L"LOG_U_%d-%d-%d %d.log", SysTime.wYear, SysTime.wMonth, SysTime.wDay, SysTime.wHour);
 
 			std::wfstream FileStream(CurrentFileName, std::ios::app);
 			if (!FileStream.is_open()) {
@@ -42,15 +36,13 @@ private:
 			return true;
 		}
 		else {
-			LPCSTR ArgcData = static_cast<LPCSTR>(Data);
+			LPCSTR Log = static_cast<LPCSTR>(LogData);
 			CHAR CurrentDate[MAX_DATETIME_LENGTH] = { "\0" };
 			CHAR CurrentFileName[MAX_PATH] = { "\0" };
 			CHAR DebugLog[MAX_BUFFER_LENGTH] = { "\0" };
-			CHAR Log[MAX_BUFFER_LENGTH] = { "\0" };
 
-			va_start(ArgcList, ArgcData);
-			vsprintf_s(Log, ArgcData, ArgcList);
-			va_end(ArgcList);
+			sprintf_s(CurrentDate, MAX_DATETIME_LENGTH, "%d-%d-%d %d:%d:%d", SysTime.wYear, SysTime.wMonth, SysTime.wDay, SysTime.wHour, SysTime.wMinute, SysTime.wSecond);
+			sprintf_s(CurrentFileName, MAX_PATH, "LOG_M_%d-%d-%d %d.log", SysTime.wYear, SysTime.wMonth, SysTime.wDay, SysTime.wHour);
 
 			std::fstream FileStream(CurrentFileName, std::ios::app);
 			if (!FileStream.is_open()) {
@@ -71,11 +63,25 @@ private:
 
 public:
 	static bool WriteLog(LPCTSTR Data, ...) {
-		return Write(true, Data);
+		TCHAR Log[MAX_BUFFER_LENGTH] = { L"\0" };
+
+		va_list ArgcList;
+		va_start(ArgcList, Data);
+		vswprintf_s(Log, Data, ArgcList);
+		va_end(ArgcList);
+
+		return Write(true, Log);
 	}
 
 	static bool WriteLog(LPCSTR Data, ...) {
-		return Write(false, Data);
+		CHAR Log[MAX_BUFFER_LENGTH] = { "\0" };
+
+		va_list ArgcList;
+		va_start(ArgcList, Data);
+		vsprintf_s(Log, Data, ArgcList);
+		va_end(ArgcList);
+
+		return Write(false, Log);
 	}
 
 };
